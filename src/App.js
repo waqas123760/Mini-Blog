e587423ad0d4b18894b4dc559c1af892a7aa4b25
waqas123760 <122608596+waqas123760@users.noreply.github.com>
2023-03-26 17:34:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import Home from "./pages/home/Home";
+import TopBar from "./components/topbar/TopBar";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
+
+function App() {
+  const { user } = useContext(Context);
+  return (
+    <Router>
+      <TopBar />
+      <Routes>
+        <Route exact path="/" element={ <Home />}>
+        </Route>
+        <Route exact path="/register" element={ user ? <Home /> : <Register />}></Route>
+
+        <Route exact path="/login" element={ user ? <Home /> : <Login />}></Route>
+
+        <Route exact path="/write" element={ user ? <Write /> : <Register />}></Route>
+        <Route exact path="/settings" element={ user ? <Settings /> : <Register />}></Route>
+        <Route exact path="/post/:postId" element={ <Single />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
